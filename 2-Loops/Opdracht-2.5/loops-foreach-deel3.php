64621e0ca1c4ad52e72535083da6f6aa3a5a6870
Din Gosto <din.gosto@student.rocva.nl>
2022-03-03 13:36:11 +0100

    klaar

---
+++ 2-Loops/Opdracht-2.5/loops-foreach-deel3.php
@@ -0,0 +1,15 @@
+<?php
+$student  = array(
+    "voornaam"    => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
+  );
+  var_dump ($student);
+  echo $student['voornaam'];
+  echo $student['achternaam'];
+  echo $student['klas'];
+  echo $student['Leeftijd'];
+  echo $student['Woonplaats'];
+?>
